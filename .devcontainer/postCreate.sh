ae26b59b80fcca3773adbd28c4f22da7ad403942
t6o_o6t <51913600+coolwind0202@users.noreply.github.com>
2026-09-10 07:13:26 +0000

    chore: suppress chpwd_recent_filehandler error

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,1 +1,4 @@
 curl https://raw.githubusercontent.com/jesseduffield/lazydocker/master/scripts/install_update_linux.sh | bash
+
+# suppress chpwd_recent_filehandler error
+mkdir -p /home/vscode/.local/share/zsh
